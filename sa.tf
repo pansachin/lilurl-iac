13d70ae464a534b72e0ce2e30ab2c76a43bcb562
Sachin Prasad <sachin.prasad@pantheon.io>
2024-10-14 21:29:20 +0530

    fmt

--- sa.tf
+++ sa.tf
@@ -1,42 +1,42 @@
 # service account for terraform in CI/CD pipeline
 resource "google_service_account" "ci-terraform" {
-    account_id = "ci-terraform"
-    display_name = "ci-terraform"
-    description = "Service account for CI/CD pipeline"
-    project = var.project_id
+  account_id   = "ci-terraform"
+  display_name = "ci-terraform"
+  description  = "Service account for CI/CD pipeline"
+  project      = var.project_id
 }
 
 resource "google_service_account_iam_member" "lilurl-iac-wiu" {
   service_account_id = google_service_account.ci-terraform.name
-  role = "roles/iam.workloadIdentityUser"
-  member = "principal://iam.googleapis.com/projects/564701988502/locations/global/workloadIdentityPools/github-wif/subject/pansachin/lilurl-iac"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/564701988502/locations/global/workloadIdentityPools/github-wif/subject/pansachin/lilurl-iac"
 }
 
 resource "google_project_iam_member" "project-owner" {
   project = var.project_id
-  role = "roles/owner"
-  member = "serviceAccount:${google_service_account.ci-terraform.email}"
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.ci-terraform.email}"
 }
 
 # service account for service deployment
 resource "google_service_account" "ci-lilurl" {
-    account_id = "ci-lilurl"
-    display_name = "ci-lilurl"
-    description = "Service account for CI/CD pipeline"
-    project = var.project_id
+  account_id   = "ci-lilurl"
+  display_name = "ci-lilurl"
+  description  = "Service account for CI/CD pipeline"
+  project      = var.project_id
 }
 
 resource "google_service_account_iam_member" "lilurl-wiu" {
   service_account_id = google_service_account.ci-lilurl.name
-  role = "roles/iam.workloadIdentityUser"
-  member = "principal://iam.googleapis.com/projects/564701988502/locations/global/workloadIdentityPools/github-wif/subject/pansachin/lilurl"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/564701988502/locations/global/workloadIdentityPools/github-wif/subject/pansachin/lilurl"
 }
 
 resource "google_project_iam_member" "lilurl-access" {
-for_each = toset([
+  for_each = toset([
   "roles/compute.admin"])
 
   project = var.project_id
-  role = each.value
-  member = "serviceAccount:${google_service_account.ci-lilurl.email}"
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.ci-lilurl.email}"
 }
