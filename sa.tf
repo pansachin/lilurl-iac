cd59ac9db4188b434ce006e93fc74b1d3b76a226
Sachin Prasad <sachin.prasad@pantheon.io>
2024-10-14 17:57:56 +0530

    lilurl SA

--- sa.tf
+++ sa.tf
@@ -1,3 +1,4 @@
+# service account for terraform in CI/CD pipeline
 resource "google_service_account" "ci-terraform" {
     account_id = "ci-terraform"
     display_name = "ci-terraform"
@@ -15,4 +16,27 @@ resource "google_project_iam_member" "project-owner" {
   project = var.project_id
   role = "roles/owner"
   member = "serviceAccount:${google_service_account.ci-terraform.email}"
+}
+
+# service account for service deployment
+resource "google_service_account" "ci-lilurl" {
+    account_id = "ci-lilurl"
+    display_name = "ci-lilurl"
+    description = "Service account for CI/CD pipeline"
+    project = var.project_id
+}
+
+resource "google_service_account_iam_member" "lilurl-wiu" {
+  service_account_id = google_service_account.ci-lilurl.name
+  role = "roles/iam.workloadIdentityUser"
+  member = "principal://iam.googleapis.com/projects/564701988502/locations/global/workloadIdentityPools/github-wif/subject/pansachin/lilurl"
+}
+
+resource "google_project_iam_member" "lilurl-access" {
+for_each = toset([
+  "roles/compute.admin"])
+
+  project = var.project_id
+  role = each.value
+  member = "serviceAccount:${google_service_account.ci-lilurl.email}"
 }
